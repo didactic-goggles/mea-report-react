2c0e30eee3925fb67327765458417d74864ab2df
didactic-goggles <58259532+developerd1320@users.noreply.github.com>
2021-07-06 00:21:33 +0300

    Commit message

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 // const fileUpload = require('express-fileupload');
 const jsonServer = require('json-server');
+const open = require('open');
 
 const middlewares = jsonServer.defaults();
 // const fs = require('fs');
@@ -31,63 +32,6 @@ if (!isDev && cluster.isMaster) {
 
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-  // app.use(fileUpload());
-
-  // app.post('/upload', function(req, res) {
-  //   if (!req.files || Object.keys(req.files).length === 0)
-  //     return res.status(400).send('No files were uploaded.');
-    
-  //   const file = req.files.file;
-  //   if(file.name.split('.').pop() != 'json')
-  //     return res.status(400).send('This is not a json file');
-  
-  //   fs.readFile(__dirname + '/data/db.json', 'utf8', function readFileCallback(err, data) {
-  //     let fileType = req.body.fileType;
-  //     if (err)
-  //       return res.status(500).send(err);
-  //     try {
-  //       const db = JSON.parse(data); //now it an object
-  //       // if(fileType == 'orders') {
-  //       //   db[fileType].push(...JSON.parse(file.data));
-  //       //   db[fileType] = db[fileType].filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i);
-  //       // }
-  //       const uploadedJSON = JSON.parse(file.data);
-  //       if(fileType == 'orders' || fileType == 'payments') {
-  //         uploadedJSON.forEach(item => {
-  //           if(item.ID) {
-  //             item.id = item.ID;
-  //             delete item.ID;
-  //           }
-  //           let compareDate = 'created' in uploadedJSON[0] ? 'created' : 'Created';
-  //           item[compareDate] = moment(item[compareDate]).unix();
-  //         })
-  //       }
-  //       db[fileType].push(...uploadedJSON);
-  //       // let compareKey = 'id' in db[fileType][0] ? 'id' : 'ID';
-  //       // db[fileType] = db[fileType].filter((v,i,a)=>a.findIndex(t=>(t[compareKey] === v[compareKey]))===i);
-  //       db.files.push({
-  //         name: file.name,
-  //         ext: file.name.split('.').pop(),
-  //         fileType: fileType,
-  //         created: new Date,
-  //         size: file.size
-  //       });
-  //       const jsString = JSON.stringify(db);
-  //       fs.writeFile(__dirname + '/data/db.json', jsString, 'utf8', function (err, data) {
-  //         if (err) {
-  //           console.log(error);
-  //           return res.status(500).send(err);
-  //         }
-  //         return res.status(200).send('Successful')
-  //       });
-  //     } catch ( error ) {
-  //       // console.log(error);
-  //       console.log(error);
-  //       return res.status(400).send('Invalid json file');
-  //     }
-  //   });
-  // });
   
   app.use('/db', middlewares, router);
 
@@ -103,3 +47,6 @@ if (!isDev && cluster.isMaster) {
     console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
   });
 }
+
+// opens the url in the default browser 
+open('http://localhost:5000');
